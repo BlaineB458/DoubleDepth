2dbd0f0e2b7e3199256f0b1a46abb0abc8205868
DoubleDepth <blaineballard@doubledepth.org>
2023-06-14 20:39:35 +0200

    Ui Changes

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Hero from '../Hero';
 import HomeServices from '../HomeServices';
+import { motion, useScroll, useTransform } from "framer-motion"
 
 const Home = () => {
+  const { scrollYProgress } = useScroll();
+  let y = useTransform(scrollYProgress, [0,1], ['-0%','-60%']);
+
   return (
     <>
+     <motion.img src="/thermfat.png" alt="" className='absolute -z-20 min-h-[850vh] md:min-h-[600vh] w-full blur-[5px]'  style={{y}} />
     <Hero/>
     <HomeServices/>
     </>
